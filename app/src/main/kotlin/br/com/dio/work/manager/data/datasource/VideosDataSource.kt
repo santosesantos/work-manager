1fcc973a9a6d9cfc76a42f31426921579100e2bd
santosesantos <bp.santosesantos@gmail.com>
2023-10-20 16:38:19 -0300

    feat: VideoDataSource for retrieving data from JSON created

---
+++ app/src/main/kotlin/br/com/dio/work/manager/data/datasource/VideosDataSource.kt
@@ -0,0 +1,19 @@
+package br.com.dio.work.manager.data.datasource
+
+import android.content.res.AssetManager
+import br.com.dio.work.manager.data.model.Video
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+object VideosDataSource {
+    private val list by lazy { arrayListOf<Video>() }
+
+    fun setFromFile(assetManager: AssetManager) {
+        val videosFromFile = Gson().fromFile<List<Video>>(assetManager, "videos.json")
+        list.addAll(videosFromFile)
+    }
+    private inline fun <reified  T> Gson.fromFile(assetManager: AssetManager, fileName: String): T {
+        return fromJson(assetManager.open(fileName).bufferedReader(), object: TypeToken<T>() {}.type)
+    }
+}
+
